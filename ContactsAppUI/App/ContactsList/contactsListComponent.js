f67fdcf2ee9e1f324cf7a5c9f7fec9c1c1d5612f
srinivasan <kksrini89@gmail.com>
2016-08-20 20:06:29 +0530

    Getting contacts information is done

--- ContactsAppUI/App/ContactsList/contactsListComponent.js
+++ ContactsAppUI/App/ContactsList/contactsListComponent.js
@@ -1,10 +1,22 @@
 ﻿(function () {
     'use strict';
     var app = angular.module('contactsApp');
+    var controller = ['contactService', function (contactService) {
+        var model = this;
+        contactService.getContacts().then(function (result) {
+            if (result.data !== null)
+                model.contacts = result.data;
+        }, function (error) {
+            console.log(error);
+        });
+        model.deleteContact = function (id) {
+            contactService.deleteContact(id);
+        }
+        //model.contacts = contactService.getContacts();
+    }];
     app.component('contactsList', {
         templateUrl: '/App/ContactsList/contactsListComponent.html',
-        controller: function () {
-            //var model = this;
-        }
+        controllerAs: 'model',
+        controller: controller
     });
 })();
--- ContactsAppUI/App/NewContact/contactService.js
+++ ContactsAppUI/App/NewContact/contactService.js
@@ -4,8 +4,20 @@
     app.service('contactService', ['$http', function ($http) {
         var contact = this;
         var url = 'http://localhost:14922/';
+        contact.getContacts = function () {
+            return $http.get(url + 'api/contact/getall');
+            //.then(function (data) {
+            //    return data;
+            //}, function (error, status, config) { console.log(error + '--' + status) });
+        }
+        contact.search = function (id) {
+            return $http.get(url + 'api/contact/get', id);
+        }
         contact.saveContact = function (newContact) {
             return $http.post(url + 'api/contact/save', newContact);
+        }
+        contact.deleteContact = function (id) {
+            $http.delete(url + 'api/contact/delete', id);
         }
         return contact;
     }]);
